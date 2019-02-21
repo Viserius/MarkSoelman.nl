0bedf85f0715f2bd98a97a17573af9d5abefc9d8
ProducerMark <SoelmanM@gmail.com>
2019-02-21 19:46:23 +0100

    Landing page design

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>MarkSoelman.nl - Home</title>
+
+    <!-- Custom CSS -->
+    <style>
+        @import url('https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700');
+
+        .text-heading {
+            font-family: 'Quicksand', sans-serif;
+            text-shadow: 3px 2px #006B98;
+            margin-bottom: -10px;
+        }
+
+        .text-huge {
+            font-size: 2.5rem;
+        }
+
+        .text-large {
+            font-size: 2rem;
+        }
+
+        html, body {
+            height: 100%;
+            overflow-y: hidden;
+        }
+
+        .bg-primary {
+            background-color: #00A1E4 !important;
+        }
+
+        .bg-primarydark {
+            background-color: #008BC5;
+        }
+
+        .icon:hover {
+            background-color: #006B98;
+        }
+    </style>
+</head>
+<body class="bg-primary">
+    <div class="container d-flex h-100 justify-content-center">
+        <div class="align-items-center align-self-center flex-column text-center">
+
+            <h1 class="text-heading text-huge title-display text-white text-sh">MarkSoelman.nl</h1>
+            <h2 class="text-large text-white mb-5">Computer Scientist</h2>
+
+            <div class="row justify-content-center">
+
+                <div class="col-6 col-md-3 my-3">
+                    <a href="https://www.linkedin.com/in/marksoelman/">
+                        <div class="bg-primarydark py-3 px-4 w-100 text-white text-large rounded icon">
+                            <i class="fab fa-linkedin-in"></i>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-6 col-md-3 my-3">
+                    <a href="https://www.twitter.com/marksoelman">
+                        <div class="bg-primarydark py-3 px-4 w-100 text-white text-large rounded icon">
+                            <i class="fab fa-twitter"></i>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-6 col-md-3 my-3">
+                    <a href="https://www.github.com/viserius">
+                        <div class="bg-primarydark py-3 px-4 w-100 text-white text-large rounded icon">
+                            <i class="fab fa-github"></i>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-6 col-md-3 my-3">
+                    <a href="mailto:info@marksoelman.nl">
+                        <div class="bg-primarydark py-3 px-4 w-100 text-white text-large rounded icon">
+                            <i class="fas fa-envelope"></i>
+                        </div>
+                    </a>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+</body>
+</html>
