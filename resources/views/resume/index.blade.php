bfd6abd928f7b9f4880dfd916195f453854d7a11
ProducerMark <SoelmanM@gmail.com>
2019-02-23 20:45:52 +0100

    progress

--- resources/views/resume/index.blade.php
+++ resources/views/resume/index.blade.php
@@ -12,7 +12,7 @@
 <body>
 
     <div class="page">
-        <div class="subpage">
+        <div class="subpage d-flex flex-column">
 
             <!-- Header -->
             <div class="container-fluid bg-dark px-0">
@@ -25,17 +25,229 @@
                         <p class="text-white name-font mb-1">MARK <span class="text-primary">SOELMAN</span></p>
                         <div class="fading-underline"></div>
                         <h2 class="text-white title-font my-0 py-0">COMPUTER SCIENTIST</h2>
-                        <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus consequatur cumque
-                            dolorem doloribus, ea in modi nulla quos? Dolorum ex, sint. Amet animi aspernatur eius magni
-                            maiores possimus quaerat ratione.
+                        <p class="text-white">I am a computer scientist and a software engineer.
+                            My expertise is in solving complex problems, and engineering efficient solutions using software languages.
+                            I have a preference for object-oriented programming languages.
+                            My native language is Dutch. I also master the English language.
+                            Currently, I am in the last year of my Bachelor in Computing Science,
+                                and looking for a new challenge for the summer of 2019.
                         </p>
                     </div>
                 </div>
 
-                <div class="horizontal-colored-border"></div>
+                <div class="horizontal-colored-border">
+                    <div class="row mx-0">
+                        <div class="col-4 p-0">
+                            <p class="text-white text-center text-smaller pt-1 mb-0">DATE OF BIRTH</p>
+                            <p class="text-white text-center text-small">7th of February 1998</p>
+                        </div>
+                        <div class="col-8 p-0">
+                            <p class="text-white text-center text-smaller pt-1 mb-0">ADDRESS</p>
+                            <p class="text-white text-center text-small">Populierenlaan 3, 7881RC Emmer-Compascuum, THE NETHERLANDS</p>
+                        </div>
+                    </div>
+                </div>
             </div>
             <!-- End of Header -->
 
+            <div class="container-fluid flex-grow-1 px-0">
+                <div class="row mx-0 h-100">
+                    <!-- Sidebar -->
+                    <div class="col-4 p-0 h-100 bg-dark">
+
+                        <!-- Contact Details -->
+                        <p class="text-primary text-right mt-3 mb-0 mr-4 ">CONTACT</p>
+                        <div class="mb-2">
+                            <div class="sidebar-header-underline-before mr-4"></div>
+                            <div class="sidebar-header-underline mr-4"></div>
+                            <div class="sidebar-header-underline-after mr-4"></div>
+                        </div>
+
+                        <!-- Mail Element -->
+                        <div class="row mt-1 mr-3">
+                            <div class="col-10 pt-1">
+                                <p class="text-light text-smaller-line text-smaller my-0 text-right">EMAIL</p>
+                                <p class="text-light text-smaller-line text-small text-right">info@marksoelman.nl</p>
+                            </div>
+                            <div class="col-2 px-0">
+                                <i class="fas fa-envelope sidebar-icon text-light text-center"></i>
+                            </div>
+                        </div>
+                        <!-- End of Mail Element -->
+
+                        <!-- LinkedIn Element -->
+                        <div class="row mt-1 mr-3">
+                            <div class="col-10 pt-1">
+                                <p class="text-light text-smaller-line text-smaller my-0 text-right">LINKEDIN</p>
+                                <p class="text-light text-smaller-line text-small text-right">linkedin.com/in/marksoelman/</p>
+                            </div>
+                            <div class="col-2 px-0">
+                                <i class="fab fa-linkedin-in sidebar-icon text-light text-center"></i>
+                            </div>
+                        </div>
+                        <!-- End of LinkedIn Element -->
+
+                        <!-- Website Element -->
+                        <div class="row mt-1 mr-3">
+                            <div class="col-10 pt-1">
+                                <p class="text-light text-smaller-line text-smaller my-0 text-right">WEBSITE</p>
+                                <p class="text-light text-smaller-line text-small text-right">linkedin.com/in/marksoelman/</p>
+                            </div>
+                            <div class="col-2 px-0">
+                                <i class="fas fa-globe sidebar-icon text-light text-center"></i>
+                            </div>
+                        </div>
+                        <!-- End of Website Element -->
+
+                        <!-- Phone Element -->
+                        <div class="row mt-1 mr-3">
+                            <div class="col-10 pt-1">
+                                <p class="text-light text-smaller-line text-smaller my-0 text-right">PHONE</p>
+                                <p class="text-light text-smaller-line text-small text-right">(+31) 6 40 21 73 29</p>
+                            </div>
+                            <div class="col-2 px-0">
+                                <i class="fas fa-mobile-alt sidebar-icon text-light text-center"></i>
+                            </div>
+                        </div>
+                        <!-- End of Phone Element -->
+
+                        <!-- End of Contact Details -->
+
+                        <!-- Start of Software Skills -->
+                        <p class="text-primary text-right mt-2 mb-0 mr-4 ">TECHNICAL SKILLS</p>
+                        <div class="mb-3">
+                            <div class="sidebar-header-underline-before mr-4"></div>
+                            <div class="sidebar-header-underline mr-4"></div>
+                            <div class="sidebar-header-underline-after mr-4"></div>
+                        </div>
+
+                        <!-- Start of OOP-Programming -->
+                        <div class="row mr-3">
+                            <div class="col-8">
+                                <p class="text-light text-smaller-line text-smaller text-right skill-title mb-0">OBJECT-ORIENTED PROGRAMMING</p>
+                            </div>
+                            <div class="col-4 px-0">
+                                <i class="circle"></i>
+                                <i class="circle"></i>
+                                <i class="circle"></i>
+                                <i class="circle"></i>
+                                <i class="circle"></i>
+                                <p class="text-white text-smaller text-smaller-line skill-description mb-0">Expert</p>
+                            </div>
+                        </div>
+                        <!-- End of OOP-Programming -->
+
+                        <!-- Start of Algorithms -->
+                        <div class="row mr-3">
+                            <div class="col-8">
+                                <p class="text-light text-smaller-line text-smaller text-right skill-title mb-0">ALGORITHMS</p>
+                            </div>
+                            <div class="col-4 px-0">
+                                <i class="circle"></i>
+                                <i class="circle"></i>
+                                <i class="circle"></i>
+                                <i class="circle"></i>
+                                <i class="circle-open"></i>
+                                <p class="text-white text-smaller text-smaller-line skill-description mb-0">Proficient</p>
+                            </div>
+                        </div>
+                        <!-- End of Algorithms -->
+
+                        <!-- Start of Data Structures -->
+                        <div class="row mr-3">
+                            <div class="col-8">
+                                <p class="text-light text-smaller-line text-smaller text-right skill-title mb-0">DATA STRUCTURES</p>
+                            </div>
+                            <div class="col-4 px-0">
+                                <i class="circle"></i>
+                                <i class="circle"></i>
+                                <i class="circle"></i>
+                                <i class="circle"></i>
+                                <i class="circle-open"></i>
+                                <p class="text-white text-smaller text-smaller-line skill-description mb-0">Proficient</p>
+                            </div>
+                        </div>
+                        <!-- End of Data Structures -->
+
+                        <!-- Start of Imperative Programming -->
+                        <div class="row mr-3">
+                            <div class="col-8">
+                                <p class="text-light text-smaller-line text-smaller text-right skill-title mb-0">IMPERATIVE PROGRAMMING</p>
+                            </div>
+                            <div class="col-4 px-0">
+                                <i class="circle"></i>
+                                <i class="circle"></i>
+                                <i class="circle"></i>
+                                <i class="circle"></i>
+                                <i class="circle-open"></i>
+                                <p class="text-white text-smaller text-smaller-line skill-description mb-0">Proficient</p>
+                            </div>
+                        </div>
+                        <!-- End of Data Structures -->
+
+                        <!-- Start of Web Development -->
+                        <div class="row mr-3">
+                            <div class="col-8">
+                                <p class="text-light text-smaller-line text-smaller text-right skill-title mb-0">WEB DEVELOPMENT</p>
+                            </div>
+                            <div class="col-4 px-0">
+                                <i class="circle"></i>
+                                <i class="circle"></i>
+                                <i class="circle"></i>
+                                <i class="circle"></i>
+                                <i class="circle-open"></i>
+                                <p class="text-white text-smaller text-smaller-line skill-description mb-0">Proficient</p>
+                            </div>
+                        </div>
+                        <!-- End of Web Development -->
+
+                        <!-- Start of FUNCTIONAL PROGRAMMING -->
+                        <div class="row mr-3">
+                            <div class="col-8">
+                                <p class="text-light text-smaller-line text-smaller text-right skill-title mb-0">FUNCTIONAL PROGRAMMING</p>
+                            </div>
+                            <div class="col-4 px-0">
+                                <i class="circle"></i>
+                                <i class="circle"></i>
+                                <i class="circle"></i>
+                                <i class="circle-open"></i>
+                                <i class="circle-open"></i>
+                                <p class="text-white text-smaller text-smaller-line skill-description mb-0">INTERMEDIATE</p>
+                            </div>
+                        </div>
+                        <!-- End of Functional Programming -->
+
+                        <!-- Start of Parallel Computing -->
+                        <div class="row mr-3">
+                            <div class="col-8">
+                                <p class="text-light text-smaller-line text-smaller text-right skill-title mb-0">PARALLEL COMPUTING</p>
+                            </div>
+                            <div class="col-4 px-0">
+                                <i class="circle"></i>
+                                <i class="circle"></i>
+                                <i class="circle-open"></i>
+                                <i class="circle-open"></i>
+                                <i class="circle-open"></i>
+                                <p class="text-white text-smaller text-smaller-line skill-description mb-0">BASIC</p>
+                            </div>
+                        </div>
+                        <!-- End of Functional Programming -->
+
+
+
+                        <!-- End of Software Skills -->
+
+                    </div>
+                    <!-- End of Sidebar -->
+
+
+                    <!-- Main Content -->
+                    <div class="col-8 p-0">
+
+                    </div>
+                    <!-- End of Main Content -->
+                </div>
+            </div>
         </div>
     </div>
     <div class="page">
